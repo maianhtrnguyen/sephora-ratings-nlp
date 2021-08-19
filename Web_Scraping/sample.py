e91e78b2fb4c4b0a93df05bd234e170433485306
Mai Anh T. Nguyen <maianhtrnguyen@gmail.com>
2021-08-19 11:25:50 -0400

    changed location of folders

--- Web_Scraping/sample.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
-url = "https://www.sephora.com/brands-list"
-
-print(requests.get(url=url, headers=headers).text)
